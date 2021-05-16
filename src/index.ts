225f98a6a93de2977e45449d6e610ead3ca25dc0
Dariusz Niemczyk <palid@phoelid.me>
2021-05-16 23:20:32 +0200

    Clean up index.ts a bit

--- src/index.ts
+++ src/index.ts
@@ -11,55 +11,79 @@ import { GLOBAL_CACHE } from './Cache';
 
 const DEBUG = false;
 
-const mapfile = path.resolve(__dirname, '../scripts.j');
-if (fs.existsSync(mapfile)) {
-  console.log(`Found mapfile at ${mapfile}.`);
+function getJassLines() {
+  const mapfile = path.resolve(__dirname, '../scripts.j');
+  if (fs.existsSync(mapfile)) {
+    console.log(`Found mapfile at ${mapfile}.`);
+  }
+  console.log(`Reading map.`);
+  const scripts = fs.readFileSync(mapfile, 'utf8');
+
+  console.log(`Splitting into lines.`);
+  const lines = scripts.split('\n');
+
+  console.log(`${lines.length} lines of JASS code found.`);
+  return lines;
 }
-console.log(`Reading map.`);
-const scripts = fs.readFileSync(mapfile, 'utf8');
 
-console.log(`Splitting into lines.`);
-const lines = scripts.split('\n');
+function matchJassLines(lines: string[]) {
+  const matchers = new Matchers();
+  matchers.registerAll([
+    HeroMatcher,
+    ItemMatcher,
+    UnitMatcher,
+    ItemDropMatcher,
+  ]);
 
-console.log(`${lines.length} lines of JASS code found.`);
-
-const matchers = new Matchers();
-matchers.registerAll([HeroMatcher, ItemMatcher, UnitMatcher, ItemDropMatcher]);
-
-for (const line of lines) {
-  const l = matchers.matchMapData(line);
-  if (l && DEBUG) {
-    console.log(`Matcher: "${l.matcher}", line: "${line}"`);
+  for (const line of lines) {
+    const l = matchers.matchMapData(line);
+    if (l && DEBUG) {
+      console.log(`Matcher: "${l.matcher}", line: "${line}"`);
+    }
   }
 }
 
-console.log('Stringifying cache');
-let cache = '';
-try {
-  cache = JSON.stringify(GLOBAL_CACHE, function replacer(key, value) {
-    if (value instanceof Map) {
-      // TODO: This probably should not be done here, but whatever lol.
-      if (key === 'itemDrop') {
-        return [...value.values()];
+function readCache() {
+  console.log('Stringifying cache');
+  let cache = '';
+  try {
+    cache = JSON.stringify(GLOBAL_CACHE, function replacer(key, value) {
+      if (value instanceof Map) {
+        // TODO: This probably should not be done here, but whatever lol.
+        if (key === 'itemDrop') {
+          return [...value.values()];
+        }
+        const results: Record<string, string> = {};
+        for (const k of value.values()) {
+          results[k.data.id] = JSON.stringify(k);
+        }
+        return results;
+      } else {
+        return value;
       }
-      const results: Record<string, string> = {};
-      for (const k of value.values()) {
-        results[k.data.id] = JSON.stringify(k);
-      }
-      return results;
-    } else {
-      return value;
-    }
-  });
-} catch (e) {
-  console.log('Cache was not stringified properly. Exiting early.');
-  throw e;
+    });
+  } catch (e) {
+    console.log('Cache was not stringified properly. Exiting early.');
+    throw e;
+  }
+  return cache;
 }
 
-console.log('Writing stringified database to file');
-try {
-  fs.writeFileSync(path.resolve(__dirname, '../build.db'), cache, 'utf8');
-} catch (e) {
-  console.log('Could not write database to file. Exiting early.');
-  throw e;
+function writeToFile(cache: string) {
+  console.log('Writing stringified database to file');
+  try {
+    fs.writeFileSync(path.resolve(__dirname, '../build.db'), cache, 'utf8');
+  } catch (e) {
+    console.log('Could not write database to file. Exiting early.');
+    throw e;
+  }
 }
+
+function main() {
+  const lines = getJassLines();
+  matchJassLines(lines);
+  const cache = readCache();
+  writeToFile(cache);
+}
+
+main();
