cb099f42e9aa18904d4b9d25db21ac4b9e22f216
Dariusz Niemczyk <palid@phoelid.me>
2021-06-01 23:03:09 +0200

    Update strings mapping

--- src/wip/testfile.ts
+++ src/wip/testfile.ts
@@ -1,15 +1,20 @@
-// import path from 'path';
-// import fs from 'fs';
-// import { ItemDropMatcher } from './matchers/ItemDropMatchers';
-// import { Matchers } from './matchers/Matcher';
+import * as path from "path";
+import * as fs from "fs";
+// import { ItemDropMatcher } from "./matchers/ItemDropMatchers";
+// import { Matchers } from "./matchers/Matcher";
 // import {
 //   HeroMatcher,
 //   ItemMatcher,
 //   UnitMatcher,
-// } from './matchers/GameObjectMatchers';
-// import { GLOBAL_CACHE, serialize } from './Cache';
-// import { matchStringsLines, WC3String } from './matchers/StringMatcher';
-// import { Line } from './matchers/Line';
+// } from "./matchers/GameObjectMatchers";
+// import { GLOBAL_CACHE, serialize } from "./Cache";
+import {
+  formatStringsDataDump,
+  matchStringsLines,
+  WC3String,
+} from "../matchers/StringMatcher";
+import { Line } from "../matchers/Line";
+import { groupBy } from "lodash/fp";
 
 // const DEBUG = false;
 
@@ -55,81 +60,30 @@
 //   }
 // }
 
-// function getStringsFile() {
-//   const stringsFile = path.resolve(__dirname, '../strings.wts');
-//   if (fs.existsSync(stringsFile)) {
-//     console.log(`Found stringsFile at ${stringsFile}.`);
-//   }
-//   console.log(`Reading map.`);
-//   const strings = fs.readFileSync(stringsFile, 'utf8');
-//   return strings;
-// }
+function getStringsFile() {
+  const stringsFile = path.resolve(__dirname, "../../mapfile/war3map.wts");
+  if (fs.existsSync(stringsFile)) {
+    console.log(`Found stringsFile at ${stringsFile}.`);
+  }
+  console.log(`Reading map.`);
+  const strings = fs.readFileSync(stringsFile, "utf8");
+  return strings;
+}
 
-// function main() {
-//   // const jassLines = getJassLines();
-//   // matchJassLines(jassLines);
+function main() {
+  // const jassLines = getJassLines();
+  // matchJassLines(jassLines);
 
-//   const strings = getStringsFile();
-//   const lines = matchStringsLines(strings);
-//   const items: Line<WC3String>[] = lines.filter(
-//     x => x?.data?.object?.type === 'item'
-//   );
+  const strings = getStringsFile();
+  const lines = matchStringsLines(strings);
+  const items: Line<WC3String>[] = lines.filter(
+    (x) => x?.data?.object?.type === "item"
+  );
 
-// const grouped = groupBy((i: Line<WC3String>) => i?.data?.object?.id)(items);
+  const mapped = formatStringsDataDump(items);
+  console.log(mapped);
+  // const cache = serialize();
+  // writeToFile(cache);
+}
 
-// const grouped = groupBy(items, (i) =>
-//   i?.data?.object?.where.replace(/ (\(.*\))/, "")
-// ) as WC3StringGroupedCollection;
-
-//   // const cache = serialize();
-//   // writeToFile(cache);
-// }
-
-// main();
-
-// type ItemObject = {
-//   name: string;
-//   description: string;
-//   ubertip: string;
-// };
-
-// function getValue(lines: Line<WC3String>[] = []) {
-//   if (lines.length > 0) {
-//     return lines[0].data?.text;
-//   }
-//   return undefined;
-// }
-
-// function getDefaultName(lines: Line<WC3String>[] = []) {
-//   if (lines.length > 0) {
-//     return lines[0].data?.object?.name;
-//   }
-//   return undefined;
-// }
-
-// type ItemObject = {
-//   name: string;
-//   description: string;
-//   ubertip: string;
-// };
-
-// function mapGroupedByIdItems(grouped: Record<string, Line<WC3String>[]>) {
-//   const group = groupBy((line: Line<WC3String>) =>
-//     line.data?.object?.where.replace(/ (\(.*\))/, "").toLowerCase()
-//   );
-//   const keys = Object.keys(grouped);
-//   const result: Record<string, Partial<ItemObject>> = {};
-//   for (const key of keys) {
-//     const items = grouped[key];
-//     const labelGrouped = group(items);
-//     const { name, description, ubertip } = labelGrouped;
-//     result[key] = {
-//       name:
-//         getValue(name) ||
-//         getDefaultName(description) ||
-//         getDefaultName(ubertip),
-//       description: getValue(description) || getValue(ubertip),
-//     };
-//   }
-//   return result;
-// }
+main();
